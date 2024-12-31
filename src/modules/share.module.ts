ce0c9303379500b2fd9b8551eb13294121c96464
Hung Hoang <hunghv2@outlook.com>
2024-12-31 21:05:54 +0700

    update code

--- src/modules/share.module.ts
+++ src/modules/share.module.ts
@@ -8,6 +8,7 @@ import { MasterData } from '../entities/masterdata.entity';
 import { MasterDataService } from '../services/masterdata.service';
 import { MasterDataController } from '../controllers/masterdata.controller';
 import { EmailService } from '../services/email.service';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
   imports: [
@@ -17,6 +18,20 @@ import { EmailService } from '../services/email.service';
     }),
     TypeOrmModule.forFeature([MasterData], 'app_db'),
     TypeOrmModule.forFeature([LoggerEntity], 'log_db'),
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.gmail.com',
+        secure: false,
+        port: 587,
+        auth: {
+          user: process.env.GMAIL_USER,
+          pass: process.env.GMAIL_APP_PASS,
+        },
+      },
+      defaults: {
+        from: '"No Reply" <noreply@duketeam.com>',
+      },
+    }),
   ],
   providers: [LoggerService, TokenService, MasterDataService, EmailService],
   exports: [LoggerService, TokenService, EmailService],
--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -1,26 +1,47 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
+import handlebars from 'handlebars';
+import { LoggerService } from './log.service';
 
 @Injectable()
 export class EmailService {
-  constructor() {}
+  constructor(
+    private readonly mailerService: MailerService,
+    private readonly loggerService: LoggerService,
+  ) {}
 
   async send(mail: any) {
-    const message = {
-      to: mail.to,
-      subject: mail.subject,
-      from: 'medusasaga@gmail.com',
-      templateId: mail.templateId,
-      dynamic_template_data: mail?.dynamicData,
-    };
+    const template = await this.loadTemplateFromDatabase(mail.templateId);
+
+    if (!template || !template.content) {
+      throw new Error('Template not found or invalid');
+    }
+
+    const compiledTemplate = handlebars.compile(template.content);
+
+    const htmlContent = compiledTemplate(mail?.dynamicData);
 
     try {
-      console.log(message);
-      console.log('Email sent successfully');
+      await this.mailerService.sendMail({
+        to: mail.to,
+        subject: mail.subject,
+        html: htmlContent,
+      });
     } catch (error) {
-      console.error(
-        'Error sending email:',
-        error.response?.body?.errors || error,
-      );
+      this.loggerService.error(error.response?.body?.errors || error);
     }
+  }
+
+  private async loadTemplateFromDatabase(
+    templateId: string,
+  ): Promise<{ content: string }> {
+    console.log(templateId);
+    return {
+      content: `
+        <h1>Welcome, {{RecipientName}}!</h1>
+        <p>Your registration is successful.  {{ConfirmUrl}}</p>
+        <p>Best regards,<br/>The Team</p>
+      `,
+    };
   }
 }
